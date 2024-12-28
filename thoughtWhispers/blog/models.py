11c4e4b336cdcaa0f5f607732f5e41e2406114bb
Harsha Yuvaraj <harshavardanyuvaraj@gmail.com>
2024-12-27 22:24:26 -0500

    Post model - Status enum class added

--- thoughtWhispers/blog/models.py
+++ thoughtWhispers/blog/models.py
@@ -2,16 +2,21 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    class Meta:
+        ordering = ['-publish', ]
+        indexes = [ models.Index(fields=['-publish']), ]
+    
+    class Status(models.TextChoices):
+        DRAFT = ('DF', 'Draft')
+        PUBLISHED = ('PB', 'Published')
+
     title = models.CharField(max_length=250)
     slug  = models.SlugField(max_length=250)
     body  = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['-publish', ]
-        indexes = [ models.Index(fields=['-publish']), ]
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
 
     def __str__(self):
         return self.title
