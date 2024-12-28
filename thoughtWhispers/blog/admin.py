54e4fd0035a225c8b99ed3271dfb9d4fd23edb08
Harsha Yuvaraj <harshavardanyuvaraj@gmail.com>
2024-12-28 18:01:08 -0500

    Admin site - Post model display improved with filters & facets

--- thoughtWhispers/blog/admin.py
+++ thoughtWhispers/blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+# User-defined models
+from .models import Post
+
+# Register models here
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    show_facets = admin.ShowFacets.ALWAYS
+
+    
+
+
