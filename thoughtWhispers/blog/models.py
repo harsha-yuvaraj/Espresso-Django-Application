3b2b3f34917d5295894e289493432db8d82836d0
Harsha Yuvaraj <harshavardanyuvaraj@gmail.com>
2025-01-01 11:29:58 -0500

    posts now have canonical urls

--- thoughtWhispers/blog/models.py
+++ thoughtWhispers/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.urls import reverse
 
 # Custom Managers
 class PublishedManager(models.Manager):
@@ -35,6 +36,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
 
 
 
