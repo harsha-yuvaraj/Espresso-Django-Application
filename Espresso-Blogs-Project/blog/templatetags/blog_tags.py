28f464b20fb3ceb3c42fe6a899d40dcf69f4b041
Harsha Yuvaraj <harshavardanyuvaraj@gmail.com>
2025-01-12 11:35:07 -0500

    Custom tempalte tag created to return the most commented posts

--- Espresso-Blogs-Project/blog/templatetags/blog_tags.py
+++ Espresso-Blogs-Project/blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 from ..models import Post
 
 # This variable will be used to register the template tags and filters of the application.
@@ -6,6 +7,7 @@ register = template.Library()
 
 @register.simple_tag
 def total_posts():
+    # returns a value indicating the total number of published posts
     return Post.published.count()
 
 @register.inclusion_tag('blog/post/latest_posts.html')
@@ -13,3 +15,7 @@ def show_latest_posts(count=3):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
 
+@register.simple_tag
+def get_most_commented_posts(count=3):
+    # returns a queryset of posts with the total number of comments for each post.
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
